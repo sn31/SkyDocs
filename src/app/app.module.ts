4a318b3724896547e186f8d6dc09cb205d255806
Meguru Yasuda <meguruyasuda@Megurus-MacBook-Air.local>
2018-11-06 10:26:34 -0800

    create landing-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule
