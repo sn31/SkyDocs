6489c0fe4acf056240cca23998858055ca1baf01
Meguru Yasuda <meguruyasuda@Megurus-MacBook-Air.local>
2018-11-07 11:49:46 -0800

    need to fix fail login logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AuthService]
 })
 export class AppComponent {
   title = 'app';
+
+  user;
+  private isLoggedIn: Boolean;
+
+  constructor(public authService: AuthService, public router: Router) {
+    this.authService.user.subscribe(user => {
+      if (user == null) {
+        this.isLoggedIn = false;
+        this.router.navigate(['']);
+      } else {
+        this.isLoggedIn = true;
+        this.router.navigate(['dash']);
+      }
+    });
+  }
 }
--- src/app/dashboard/dash-side/dash-side.component.ts
+++ src/app/dashboard/dash-side/dash-side.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-dash-side',
   templateUrl: './dash-side.component.html',
-  styleUrls: ['./dash-side.component.css']
+  styleUrls: ['./dash-side.component.css'],
+  providers: [AuthService]
+
 })
 export class DashSideComponent implements OnInit {
 
 
-  constructor() { }
+  constructor(public authService: AuthService) { }
+
+  logout() {
+    this.authService.logout();
+  }
 
   ngOnInit() {
   }
